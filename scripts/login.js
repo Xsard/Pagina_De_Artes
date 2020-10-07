721a84090307bec352b12eab1637d89f8eb00ec5
Xsard <65749633+Xsard@users.noreply.github.com>
2020-10-06 22:10:23 -0300

    Versión 0.2.2
    Cambios en el login y el logout

--- scripts/login.js
+++ scripts/login.js
@@ -1,10 +1,15 @@
 function login(evt) {
     var email = document.getElementById("InputEmail").value;
     var pass = document.getElementById("InputPassword").value;
-    if (email == "a@a" & pass=="a") {
-        location.href = "home.html"
-        evt.preventDefault();
+    if (email == "a@a" & pass == "a") {
+        window.close();
+        window.open("homeloged.html")
     } else {
         alert("El correo y la contraseña no coinciden")
     }
+}
+
+function logout(){
+    window.close();
+    window.open("login.html")
 }
