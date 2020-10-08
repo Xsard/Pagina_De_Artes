52e927b59da290992a1fb1bb32c6ff930cfc30bd
Xsard <65749633+Xsard@users.noreply.github.com>
2020-10-07 22:48:13 -0300

    V 0.3.4
    Validaciones para registrarse en la web

--- scripts/signup.js
+++ scripts/signup.js
@@ -5,12 +5,22 @@ function validaciones(evt) {
     var emailCont = document.getElementById("EmailCont").value;
     var nombres = document.getElementById("Nombres").value;
     var NomArt = document.getElementById("NomArt").value;
-    var formulario = document.getElementById("")
-    if (pass.length == 0 || /^\s+$/.test(pass)) {
+    var Edad = document.getElementById("Edad").value;
+    if (pass.length == 0 || /^\s+$/.test(pass) || email.length == 0 || /^\s+$/.test(email) || emailCont.length == 0 || /^\s+$/.test(emailCont) || nombres.length == 0 || /^\s+$/.test(nombres) || passCom.length == 0 || /^\s+$/.test(passCom) || NomArt.length == 0 || /^\s+$/.test(NomArt) || Edad.length == 0 || /^\s+$/.test(Edad)) {
         alert("No pueden haber campos vacios")
-        evt.preventDefault();               
+        evt.preventDefault();
     } else {
-        alert("a")
+        if (pass === passCom) {
+            if (Edad < 8 || isNaN(parseInt(Edad))) {
+                alert("La edad no es un número o es menor a 18")
+                evt.preventDefault();
+            } else {
+                alert("a")
+            }
+        } else {
+            alert("Las contraseñas no coinciden")
+            evt.preventDefault();
+        }
     }
 
 }
